a810c3d1def3e84608ba4a3370fe1f4dd4b7b9a1
Earl Panganiban <earl@quadrivium.xyz>
2021-06-21 11:52:21 +0800

    Changed ami to type available in ap-northeast-2

--- terraform-associate/04-learn-terraform-aws-instance/main.tf
+++ terraform-associate/04-learn-terraform-aws-instance/main.tf
@@ -15,7 +15,7 @@ provider "aws" {
 }
 
 resource "aws_instance" "app_server" {
-  ami           = "ami-830c94e3"
+  ami           = "ami-02af1a55fd4da5ab1"
   instance_type = "t2.micro"
 
   tags = {
