f2fae48c20592640cfa32f504a91daf883add1d2
Earl Panganiban <earl@quadrivium.xyz>
2021-06-21 12:37:45 +0800

    Added remote backend to main.tf; configured for remote execution on terraform cloud

--- terraform-associate/04-learn-terraform-aws-instance/main.tf
+++ terraform-associate/04-learn-terraform-aws-instance/main.tf
@@ -1,4 +1,10 @@
 terraform {
+  backend "remote" {
+      organization = "Quadrivium"
+      workspaces {
+          name = "hashicorp-learn"
+      }
+  }
   required_providers {
     aws = {
       source  = "hashicorp/aws"
