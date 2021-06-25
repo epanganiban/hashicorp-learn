b3cef8616dcc148312b1136dda118ce63f8198a4
Earl Panganiban <earl@quadrivium.xyz>
2021-06-25 16:25:57 +0800

    Added env and ports to docker_container resource

--- terraform-associate/15-learn-terraform-import/main.tf
+++ terraform-associate/15-learn-terraform-import/main.tf
@@ -15,4 +15,14 @@ provider "docker" {
   host = "tcp://localhost:2375/"
 }
 
-resource "docker_container" "web" {}
+resource "docker_container" "web" {
+  name  = "hashicorp-learn"
+  image = "sha256:4f380adfc10f4cd34f775ae57a17d2835385efd5251d6dfe0f246b0018fb0399"
+
+  env = []
+
+  ports {
+    external = 8080
+    internal = 80
+  }
+}
