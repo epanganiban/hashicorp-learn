3e38a7644d8d79d5d95c0c15134e27e8fed3817b
Earl Panganiban <earl@quadrivium.xyz>
2021-06-26 11:04:24 +0800

    Changed region to ap-northeast-2; ami hash pulled dynamically

--- terraform-associate/20-learn-terraform-modules-use/main.tf
+++ terraform-associate/20-learn-terraform-modules-use/main.tf
@@ -9,7 +9,17 @@ terraform {
 }
 
 provider "aws" {
-  region = "us-west-2"
+  region = "ap-northeast-2"
+}
+
+data "aws_ami" "amazon_linux" {
+  most_recent = true
+  owners      = ["amazon"]
+
+  filter {
+    name   = "name"
+    values = ["amzn2-ami-hvm-*-x86_64-gp2"]
+  }
 }
 
 module "vpc" {
@@ -35,7 +45,7 @@ module "ec2_instances" {
   name           = "my-ec2-cluster"
   instance_count = 2
 
-  ami                    = "ami-0c5204531f799e0c6"
+  ami                    = data.aws_ami.amazon_linux.id
   instance_type          = "t2.micro"
   vpc_security_group_ids = [module.vpc.default_security_group_id]
   subnet_id              = module.vpc.public_subnets[0]
